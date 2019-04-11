b2a97863d9f763915fe19d50b99cc84d11fe0125
alvalle09 <phredvalle@gmail.com>
2019-04-10 23:50:27 -0500

    attempt at changing from styl to css

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -4,6 +4,6 @@ import React from 'react';
 import { render } from 'react-dom';
 
 // import css
-import css from './styles/style.styl';
+import css from './styles/style.css';
 
 render(<p>hewo</p>, document.getElementById('root'));
